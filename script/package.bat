71e23b9f84c2f8c99d919d464bfe21f0359d2be2
Guilherme Parmezani <g.parmezani@gmail.com>
2014-11-07 18:55:28 -0200

    Commit: VTG-00 Branch: V3.0.1 Description: Package script version adjustments.

--- script/package.bat
+++ script/package.bat
@@ -4,28 +4,28 @@ echo Criando pasta Webapp...
 mkdir "C:\Program Files (x86)\VertrigoServ\www\webapp"
 echo.
 echo Copiando pasta webapp...
-xcopy C:\Projetos\voting\voting-3.0.0\webapp "C:\Program Files (x86)\VertrigoServ\www\webapp" /s /y
+xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\webapp "C:\Program Files (x86)\VertrigoServ\www\webapp" /s /y
 echo.
 
 echo Criando pasta dao...
 mkdir "C:\Program Files (x86)\VertrigoServ\www\dao"
 echo.
 echo Copiando pasta dao...
-xcopy C:\Projetos\voting\voting-3.0.0\dao "C:\Program Files (x86)\VertrigoServ\www\dao" /s /y
+xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\dao "C:\Program Files (x86)\VertrigoServ\www\dao" /s /y
 echo.
 
 echo Criando pasta model...
 mkdir "C:\Program Files (x86)\VertrigoServ\www\model"
 echo.
 echo Copiando pasta model...
-xcopy C:\Projetos\voting\voting-3.0.0\model "C:\Program Files (x86)\VertrigoServ\www\model" /s /y
+xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\model "C:\Program Files (x86)\VertrigoServ\www\model" /s /y
 echo.
 
 echo Criando pasta util...
 mkdir "C:\Program Files (x86)\VertrigoServ\www\util"
 echo.
 echo Copiando pasta util...
-xcopy C:\Projetos\voting\voting-3.0.0\util "C:\Program Files (x86)\VertrigoServ\www\util" /s /y
+xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\util "C:\Program Files (x86)\VertrigoServ\www\util" /s /y
 echo.
 
 echo Criando pasta graph...
@@ -36,7 +36,7 @@ echo.
 ::mkdir "C:\Program Files (x86)\VertrigoServ\www\webapp\jpgraph"
 ::echo.
 ::echo Copiando pasta jpgraph...
-::xcopy C:\Projetos\voting\voting-3.0.0\jpgraph "C:\Program Files (x86)\VertrigoServ\www\jpgraph" /s /y
+::xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\jpgraph "C:\Program Files (x86)\VertrigoServ\www\jpgraph" /s /y
 ::echo.
 
 IF ERRORLEVEL 1 (
@@ -45,12 +45,12 @@ IF ERRORLEVEL 1 (
 )
 
 echo Copiando index.php...
-xcopy C:\Projetos\voting\voting-3.0.0\index.php "C:\Program Files (x86)\VertrigoServ\www" /y
+xcopy C:\Projetos\voting\voting-3.0.1-GIT\voting-3.0.1\index.php "C:\Program Files (x86)\VertrigoServ\www" /y
 echo.
 
 ::echo Gerando arquivo de deploy .ZIP
-::cd C:\Projetos\voting\voting-3.0.0
-::7z a -oc:\projetos\voting\voting-3.0.0 deploy.zip
+::cd C:\Projetos\voting\voting-3.0.1-GIT
+::7z a -oc:\projetos\voting\voting-3.0.1-GIT deploy.zip
 ::7z d deploy.zip .settings assets database script .project deploy.zip
 ::echo.
 
